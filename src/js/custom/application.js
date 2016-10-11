246f5e169fd1581dd7576acd205471af32f507b9
Jake Apple <jake.apple@secondstreet.com>
2016-10-10 23:34:31 -0500

    getting closer

--- src/js/custom/application.js
+++ src/js/custom/application.js
@@ -7,9 +7,11 @@ var App = {
     App._tooltips()
     App._quickLinksSearch()
     App._calendar()
+    App._dataTables()
 
     $(window).on('resize', App._tooltips)
     $(window).on('resize', App._calendar)
+    //$(window).on('resize', App._dataTables.dtTables.refreshUi)
 
     $(document).on('shown.bs.tab', function () {
       $(document).trigger('redraw.bs.charts')
@@ -51,6 +53,40 @@ var App = {
      } else{
        $('#calendar').fullCalendar('option', 'aspectRatio', .9)
      }
+   },
+
+   _dataTables: function() {
+     $.extend( $.fn.dataTable.defaults, {
+         searching: false,
+         lengthChange: false,
+         paging: false,
+         info: false
+     } );
+     var dtTables = $('.table-datatable').DataTable( {
+         responsive: {
+             details: {
+               renderer: function ( api, rowIdx, columns ) {
+                   var data = $.map( columns, function ( col, i ) {
+                       return col.hidden ?
+                           '<div class="row" data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
+                               '<div class="col-sm-6">'+col.title+':'+'</div> '+
+                               '<div class="col-sm-6">'+col.data+'</div>'+
+                           '</div>' :
+                           '';
+                   } ).join('');
+
+                   return data ?
+                       $('<div/>').append( data ) :
+                       false;
+               }
+             }
+         }
+     } );
+    //  dtTables.prototype.refreshUi = function() {
+    //     console.log("Redrawn!");
+    //     $.dataTable.columns.adjust().draw();
+    //     $.dataTable.responsive.recalc();
+    //  };
    }
  }
 
