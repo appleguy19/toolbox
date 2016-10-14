c4fb2f0d4856018d66287a84f91a83e43335f1cd
Jake Apple <jake.apple@secondstreet.com>
2016-10-14 10:22:29 -0500

    Progress after a late night

--- src/js/custom/application.js
+++ src/js/custom/application.js
@@ -6,11 +6,9 @@ var App = {
     App._tableSorters()
     App._tooltips()
     App._quickLinksSearch()
-    App._dataTables()
 
     $(window).on('resize', App._tooltips)
-    $(window).on('resize', App._calendar)
-    //$(window).on('resize', App._dataTables.dtTables.refreshUi)
+    $(window).on('resize', App._dataTables)
 
     $(document).on('shown.bs.tab', function () {
       $(document).trigger('redraw.bs.charts')
@@ -37,49 +35,7 @@ var App = {
 
   _quickLinksSearch: function() {
     $('#quick-links-search').quickSearch($('#quick-links-content'));
-  },
-
-   _dataTables: function() {
-     $.extend( $.fn.dataTable.defaults, {
-         searching: false,
-         lengthChange: false,
-         paging: false,
-         info: false
-     } );
-     var dtTables = $('.table-datatable').DataTable( {
-         responsive: {
-             details: {
-               type: 'column',
-               target: 'tr',
-               renderer: function ( api, rowIdx, columns ) {
-                   var data = $.map( columns, function ( col, i ) {
-                       return col.hidden ?
-                           '<div class="row" data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
-                               '<div class="col-sm-6">'+col.title+':'+'</div> '+
-                               '<div class="col-sm-6">'+col.data+'</div>'+
-                           '</div>' :
-                           '';
-                   } ).join('');
-
-                   return data ?
-                       $('<div/>').append( data ) :
-                       false;
-               },
-               display: $.fn.dataTable.Responsive.display.modal( {
-                header: function ( row ) {
-                    var data = row.data();
-                    return 'Details for '+data.clientName;
-                }
-               } )
-             }
-         }
-     } );
-    //  dtTables.prototype.refreshUi = function() {
-    //     console.log("Redrawn!");
-    //     $.dataTable.columns.adjust().draw();
-    //     $.dataTable.responsive.recalc();
-    //  };
-   }
+  }
  }
 
 App.init()
